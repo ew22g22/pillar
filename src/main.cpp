e3d381f9304ff46ed03035b804966d21c3fcfdd3
ew22g22 <ew22g22@soton.ac.uk>
2025-12-27 21:12:33 +0000

    Added simple starting files

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <print>
+
+auto main(int argc, char **argv) -> int {
+    static auto constexpr PROJECT_NAME = "Pillar";
+    
+    std::println("Hello from {}.", PROJECT_NAME);
+    return 0;
+}
